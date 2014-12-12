2633e920b73b5af3c883ea7f4db37fe23066ff7c
Jesus Aller <jesusallerdiez@gmail.com>
2014-12-12 12:05:54 +0100

    Creado un sistema basico de informacion nutricional, donde se pueden introducir datos de macronutrientes, tiene el metodo muestrDatos donde imprime por pantalla la informacion nutricional, en el constructor se calcula la cantidad de calorias segun la cantidad de macronutrientes

---
+++ Alimento.java
@@ -0,0 +1,63 @@
+public class Alimento
+{
+    //La cantidad de proteina
+    private float proteinas;
+    //La cantidad de grasas
+    private float grasas;
+    //La cantidad de carbohidratos
+    private float carbohidratos;
+    //El nombre del alimento
+    private String nombre;
+    //Las calorias dependen de la cantidad de macronutrientes
+    private float calorias;
+
+    /**
+     * Contructor donde daremos el valor inicial del objeto alimento
+     */
+    public Alimento(float proteinas, float grasas, float carbohidratos, String nombre)
+    {
+        this.proteinas = proteinas;
+        this.grasas = grasas;
+        this.carbohidratos = carbohidratos;
+        this.nombre = nombre;
+        calorias = (proteinas * 4) + (grasas * 9) + (carbohidratos * 4);
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     */
+    public void muestraDatos ()
+    {
+        System.out.println("Nombre: " + nombre + "");
+        System.out.println("Proteinas por cada 100 gramos: " + proteinas + "");
+        System.out.println("Carbohidratos por cada 100 gramos: " + carbohidratos + "");
+        System.out.println("Grasas por cada 100 gramos: " + grasas + "");
+        System.out.println("Calorias: " + calorias + "");
+        if (proteinas >= grasas)
+        {
+            if (grasas > carbohidratos)
+            {
+                System.out.println("Componente/s mayoritario/s: Proteinas y grasas");
+            }
+            else
+            {
+                System.out.println("Componente/s mayoritario/s: Proteinas y carbohidratos");
+            }
+        }
+        else if (grasas >= proteinas)
+        {
+            if (proteinas > carbohidratos)
+            {
+                System.out.println("Componente/s mayoritario/s: Grasas y proteinas");
+            }
+            else
+            {
+                System.out.println("Componente/s mayoritario/s: Grasas y carbohidratos");
+            }
+        }
+        else if (grasas == proteinas && carbohidratos == proteinas)
+        {
+            System.out.println("Componente/s mayoritario/s: Ninguno en especial");
+        }
+    }
+}
